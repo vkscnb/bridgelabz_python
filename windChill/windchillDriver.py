de8acc2edfcd3b06cfd9e1f9244f7780466e21be
Vikas Sharma <vkscnb@gmail.com>
2019-12-16 14:46:31 +0530

    [Vikas Sharma]modify wind chill program

--- windChill/windchillDriver.py
+++ windChill/windchillDriver.py
@@ -1,3 +1,8 @@
+#***********************************************
+#@purpose :find wind is chill or not
+#@file    :windchillDriver.py
+#@author  :Vikas Sharma
+#***********************************************
 from sys import argv
 
 from utility.utilMethods import is_windchill
