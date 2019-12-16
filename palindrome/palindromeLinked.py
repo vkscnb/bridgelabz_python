a11aff87b29cd7880535d33d8bc0866cbd16962b
Vikas Sharma <vkscnb@gmail.com>
2019-12-16 13:53:11 +0530

    [Vikas Sharma]modify palindrome program

--- palindrome/palindromeLinked.py
+++ palindrome/palindromeLinked.py
@@ -1,3 +1,8 @@
+#***********************************************
+#@purpose :check linked list is palindrome or not
+#@file    :palindromeLinked.py
+#@author  :Vikas Sharma
+#***********************************************
 from utility.utilMethods import is_palindrome
 
 str = input("enter the string\n")
