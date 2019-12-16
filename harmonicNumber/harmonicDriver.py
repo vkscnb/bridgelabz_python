2a2800c6dc2820afd7f693d3b415546d293ec0f2
Vikas Sharma <vkscnb@gmail.com>
2019-12-16 14:39:22 +0530

    [Vikas Sharma]modify harmonic number program

--- harmonicNumber/harmonicDriver.py
+++ harmonicNumber/harmonicDriver.py
@@ -1,3 +1,8 @@
+#***********************************************
+#@purpose :harmonic series
+#@file    :harmonicDriver.py
+#@author  :Vikas Sharma
+#***********************************************
 from utility.utilMethods import is_harmonic
 
 num =int(input("enter the number"))
