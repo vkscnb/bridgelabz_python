68e84c4817e690bf29af0f001a9edb1b61272f60
Vikas Sharma <vkscnb@gmail.com>
2019-12-16 14:22:24 +0530

    [Vikas Sharma]modify coupon number program

--- couponNumber/couponDriver.py
+++ couponNumber/couponDriver.py
@@ -1,3 +1,8 @@
+#***********************************************
+#@purpose :generate random coupon number
+#@file    :couponDriver.py
+#@author  :Vikas Sharma
+#***********************************************
 from utility.utilMethods import is_coupon
 
 num = int(input("enter how many coupon you want" ))
