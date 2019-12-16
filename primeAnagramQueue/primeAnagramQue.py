a54d1079f9b1e82c92918daf2047851a33e6ae16
Vikas Sharma <vkscnb@gmail.com>
2019-12-16 14:00:56 +0530

    [Vikas Sharma]modify primeAnagramqueue  program

--- primeAnagramQueue/primeAnagramQue.py
+++ primeAnagramQueue/primeAnagramQue.py
@@ -1,3 +1,8 @@
+#***********************************************************************************
+#@purpose :find prime no and find find anagram of prime number and stored in a queue 
+#@file    :primeAnagramQueue.py
+#@author  :Vikas Sharma
+#***********************************************************************************
 from utility.utilMethods import is_primeQueue
 
 Range=int(input("Enter the number of Range"))
