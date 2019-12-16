bf2d00e4852c64f83e1a0aaa2edc6bd6c89e5cab
Vikas Sharma <vkscnb@gmail.com>
2019-12-16 14:34:34 +0530

    [vikas Sharma]modify flip coin program

--- flipCoin/flipcoinDriver.py
+++ flipCoin/flipcoinDriver.py
@@ -1,3 +1,8 @@
+#***********************************************
+#@purpose :probability of a coin
+#@file    :flipcoinDriver.py
+#@author  :Vikas Sharma
+#***********************************************
 from utility.utilMethods import is_flipcoin
 
 num=int(input("enter the number how many time dice flip "))
