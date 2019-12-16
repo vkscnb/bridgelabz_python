2a1bf4f231a63822f78847818dc9a32794d7bb34
Vikas Sharma <vkscnb@gmail.com>
2019-12-16 14:31:14 +0530

    [Vikas Sharma]modify factor number program

--- factorNumber/factorDriver.py
+++ factorNumber/factorDriver.py
@@ -1,3 +1,8 @@
+#***********************************************
+#@purpose :factor of a number
+#@file    :factorDriver.py
+#@author  :Vikas Sharma
+#***********************************************
 from utility.utilMethods import is_primeFactors
 print('enter the num to find the prime factor')
 num = int(input())
