d254f7193e40c6128430e9e9903dacba52003f30
Vikas Sharma <vkscnb@gmail.com>
2019-12-16 14:05:46 +0530

    [Vikas Sharma]modify queueCash program

--- queueCash/queueBanking.py
+++ queueCash/queueBanking.py
@@ -1,3 +1,8 @@
+#***********************************************
+#@purpose :store bank record in a queue
+#@file    :queueBanking.py
+#@author  :Vikas Sharma
+#***********************************************
 from utility.utilMethods import is_queue
 
 people = int(input("enter how many people in a queue"))
