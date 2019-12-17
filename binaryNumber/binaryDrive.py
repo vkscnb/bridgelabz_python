959ee95cb2efeb06fc48f00ba73f86f745590755
Vikas Sharma <vkscnb@gmail.com>
2019-12-17 10:09:51 +0530

    [Vikas Sharma]Rename and Modify. rename the class name and modify the program

--- binaryNumber/binaryDrive.py
+++
@@ -1,4 +0,0 @@
-from utility.utilMethods import is_binarynum
-
-binnum = int(input("enter the number to find Binary"))
-is_binarynum(binnum)
---
+++ binaryNumber/decimal2binary.py
@@ -0,0 +1,10 @@
+#**************************************************
+#@purpose : convert decimal number to binary number
+#@file    : decimal2binary.py
+#@author  : Vikas Sharma
+#**************************************************
+
+from utility.utilMethods import is_binarynum
+
+binnum = int(input("enter the number to find Binary"))
+is_binarynum(binnum)
