f1230cafc5a5bb5758748e3fcfdf4fb36c05c230
Vikas Sharma <vkscnb@gmail.com>
2019-12-17 10:48:01 +0530

    [Vikas Sharma]Rename and Modify. rename the class name and modify the program

--- insertionString/insertionDriver.py
+++ insertionString/insertionDriver.py
@@ -1,14 +1,20 @@
+#***********************************************
+#@purpose :program for Insertion sort
+#@file    :insertion_sort.py
+#@author  :Vikas Sharma
+#***********************************************
+
 from utility.utilMethods import is_insertion
 
-num = int(input("enter the size of the list"))
-name=[]
+lst_size = int(input("enter the size of the list"))
+name = []
 
-while num>=0:
+while lst_size >= 0:
     print("enter the name in the list")
-    sname=input()
+    sname = input()
     name.append(sname)
-    num-=1
+    num = num -1
 print(name)
 print()
-name2=is_insertion(name)
+name2 = is_insertion(name)
 print(name2)
