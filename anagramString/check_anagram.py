7a8737253d9447e376121d1fe162645acf48bda0
Vikas Sharma <vkscnb@gmail.com>
2019-12-17 10:02:22 +0530

    [Vikas Sharma]Rename and Modify. rename the class name and modify the program

---
+++ anagramString/check_anagram.py
@@ -0,0 +1,11 @@
+#***********************************************
+#@purpose :check anagram to string
+#@file    :check_anagram.py
+#@author  :Vikas Sharma
+#***********************************************
+
+from utility.utilMethods import is_anagram
+
+str1 = input("enter the first string")
+str2 = input("enter the second string")
+is_anagram(str1,str2)
