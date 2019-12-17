bd86360bdac74556696c36bbfd96d2e6bc56cabb
Vikas Sharma <vkscnb@gmail.com>
2019-12-17 10:16:49 +0530

    [Vikas Sharma]Rename and Modify. rename the class name and modify the program

--- binarySearch/binary_search.py
+++ binarySearch/binary_search.py
@@ -1,34 +1,43 @@
-from utility.utilMethods import is_binarySearch
-#search for string
-str=(int(input("enter the choice to binary search any number for string  or 0 for number binary search")))
+#***********************************************
+#@purpose :Binary search for string and number
+#@file    :binary_search.py
+#@author  :Vikas Sharma
+#***********************************************
 
-if str>0:
+from utility.utilMethods import is_binarySearch
+
+#search for string
+
+str = (int(input("enter the choice to binary search any number for string  or 0 for number binary search")))
+
+if str > 0:
     num = int(input("enter the size of the list in a string"))
-    arr=[]
-    while num>0:
-        lst=input("enter the element")
+    arr = []
+    while num > 0:
+        lst = input("enter the element")
         arr.append(lst)
-        num-=1
+        num = num-1
     print(arr)
     search = input("enter the string element to search in the list")
     result = is_binarySearch(arr, search)
-    if (result == -1):
+    if result is -1:
         print("Element not present")
     else:
         print("Element found at index",result)
 
 #search for integer
+
 else:
     num1 = int(input("enter the size of the list in a string"))
     arr1 = []
     while num1 > 0:
         lst1 = input("enter the element")
         arr1.append(lst1)
-        num1 -= 1
+        num1 = num1 - 1
     intsearch = input("enter the element to search in the integer list")
     result1 = is_binarySearch((arr1,intsearch))
     print(result1)
-    if (result1 == -1):
+    if result1 is -1:
         print("Element not present")
     else:
         print("Element found at index",result1)
