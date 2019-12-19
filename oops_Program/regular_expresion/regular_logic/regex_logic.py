e524a39215cc3b33c9eb06ba8a00f3c511020325
Vikas Sharma <vkscnb@gmail.com>
2019-12-19 12:31:27 +0530

    [Vikas sharma]Modify. Modify regular expresion program in oops_program

--- oops_Program/regular_expresion/regular_logic/regex_logic.py
+++ oops_Program/regular_expresion/regular_logic/regex_logic.py
@@ -5,19 +5,29 @@
 #*************************************************************************
 
 import re
-from datetime import datetime
+import datetime
 
-read_file = open("/home/user/Videos/test/bridgelabz_python/oops_Program/regular_expresion/text_file/text.txt", "r")
-write_file = open("/home/user/Videos/test/bridgelabz_python/oops_Program/regular_expresion/text_file/write.txt", "w")
-
+read_file = open("/home/user/Videos/test/bridgelabz_python/oops_Program/regular_expresion/text_file/text.txt", "r+")
+line = read_file.read() 
+print(line
+)
 first_name = input("Enter the firstname")
 full_name = input("Enter the full name")
+mobile_number = input("enter the mobile number with ")
 
-for line in read_file:
-    match_first_name = re.sub('<<name>>', first_name, line)
-    match_full_name = re.sub('<<full name>>', full_name, line)
-    match_mobile_number = re.sub('91-[x]{10}', '91-[0-9]{10}', line)
-    match_date = re.sub('[0-2][0-9]|/[0-1][0-2]/[0-9]{4}', first_name, line)
+ 
+data_update = re.sub('[<]{2}[a-zA-Z]{4}[>]{2}', first_name, line)
+data_update = re.sub('[<]{2}[a-zA-Z]{8}[>]{2}', full_name, data_update)
+data_update = re.sub('[x]{10}', mobile_number, data_update)
+
+date_today = str(datetime.date.today())
+data_update = re.sub('01/01/2016', date_today ,data_update)
+
+output_data = open("/home/user/Videos/test/bridgelabz_python/oops_Program/regular_expresion/text_file/write.txt", "w")
+output_data.write(data_update)
+
+read_file.close()
+output_data.close()
 
 
-print(datetime.now())
+
