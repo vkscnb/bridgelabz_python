6675d00509657efed4d10cff88bdf4a4345c96de
Vikas Sharma <vkscnb@gmail.com>
2019-12-19 12:37:50 +0530

    [Vikas sharma]Comment. commenting  the  regular expresion program in oops_program folder

--- oops_Program/regular_expresion/regular_logic/regex_logic.py
+++ oops_Program/regular_expresion/regular_logic/regex_logic.py
@@ -7,15 +7,18 @@
 import re
 import datetime
 
+# read the data from file 
+
 read_file = open("/home/user/Videos/test/bridgelabz_python/oops_Program/regular_expresion/text_file/text.txt", "r+")
 line = read_file.read() 
-print(line
-)
+
+# enter the fist name, full name, and mobile number from the user
+
 first_name = input("Enter the firstname")
 full_name = input("Enter the full name")
 mobile_number = input("enter the mobile number with ")
 
- 
+#check the regular expression and replace the user input value 
 data_update = re.sub('[<]{2}[a-zA-Z]{4}[>]{2}', first_name, line)
 data_update = re.sub('[<]{2}[a-zA-Z]{8}[>]{2}', full_name, data_update)
 data_update = re.sub('[x]{10}', mobile_number, data_update)
@@ -23,9 +26,12 @@ data_update = re.sub('[x]{10}', mobile_number, data_update)
 date_today = str(datetime.date.today())
 data_update = re.sub('01/01/2016', date_today ,data_update)
 
+# open the second file for write the updated data
+
 output_data = open("/home/user/Videos/test/bridgelabz_python/oops_Program/regular_expresion/text_file/write.txt", "w")
 output_data.write(data_update)
 
+# close the all file 
 read_file.close()
 output_data.close()
 
