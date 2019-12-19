f1abde7608a1f3d2af1a50342706a607c9ffac50
Vikas Sharma <vkscnb@gmail.com>
2019-12-19 12:41:45 +0530

    [Vikas sharma]Modify. Modify regular expresion program in oops_program

--- oops_Program/regular_expresion/regular_logic/regex_logic.py
+++ oops_Program/regular_expresion/regular_logic/regex_logic.py
@@ -7,33 +7,39 @@
 import re
 import datetime
 
-# read the data from file 
+class RegularExpression:
 
-read_file = open("/home/user/Videos/test/bridgelabz_python/oops_Program/regular_expresion/text_file/text.txt", "r+")
-line = read_file.read() 
+    def regular_expression(self):
+            
+        # read the data from file 
 
-# enter the fist name, full name, and mobile number from the user
+        read_file = open("/home/user/Videos/test/bridgelabz_python/oops_Program/regular_expresion/text_file/text.txt", "r+")
+        line = read_file.read() 
 
-first_name = input("Enter the firstname")
-full_name = input("Enter the full name")
-mobile_number = input("enter the mobile number with ")
+        # enter the fist name, full name, and mobile number from the user
 
-#check the regular expression and replace the user input value 
-data_update = re.sub('[<]{2}[a-zA-Z]{4}[>]{2}', first_name, line)
-data_update = re.sub('[<]{2}[a-zA-Z]{8}[>]{2}', full_name, data_update)
-data_update = re.sub('[x]{10}', mobile_number, data_update)
+        first_name = input("Enter the firstname")
+        full_name = input("Enter the full name")
+        mobile_number = input("enter the mobile number with ")
 
-date_today = str(datetime.date.today())
-data_update = re.sub('01/01/2016', date_today ,data_update)
+        #check the regular expression and replace the user input value 
+        data_update = re.sub('[<]{2}[a-zA-Z]{4}[>]{2}', first_name, line)
+        data_update = re.sub('[<]{2}[a-zA-Z]{8}[>]{2}', full_name, data_update)
+        data_update = re.sub('[x]{10}', mobile_number, data_update)
 
-# open the second file for write the updated data
+        date_today = str(datetime.date.today())
+        data_update = re.sub('01/01/2016', date_today ,data_update)
 
-output_data = open("/home/user/Videos/test/bridgelabz_python/oops_Program/regular_expresion/text_file/write.txt", "w")
-output_data.write(data_update)
+        # open the second file for write the updated data
 
-# close the all file 
-read_file.close()
-output_data.close()
+        output_data = open("/home/user/Videos/test/bridgelabz_python/oops_Program/regular_expresion/text_file/write.txt", "w")
+        output_data.write(data_update)
 
+        # close the all file 
+        read_file.close()
+        output_data.close()
+
+regular_express = RegularExpression()
+regular_express.regular_expression
 
 
