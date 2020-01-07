72defe05b36335f2868b4a8572678e7f838fdad1
kishorekumar646 <kishorekumar131646@gmai.com>
2020-01-07 11:02:02 +0530

    [Vikas Sharma]Add.added

---
+++ oops_Program/stock_report/stock_manage.py
@@ -0,0 +1,27 @@
+#*************************************************************************
+#@purpose : Read the Json file and calculate the stock price 
+#@file : stock_manage.py
+#@author: Vikas Sharma
+#*************************************************************************
+
+#importing json file
+
+import json
+
+class Stock:
+
+    def getStockDetails(self):
+        #Reading JSON from a File
+        with open("/home/user/Videos/test/bridgelabz_python/oops_Program/stock_report/stock_record.json","r") as f:
+
+            #read string from JSON & storing in variable
+            data1= json.load(f)
+            #storing key
+            data = data1["Stock_report"]
+        return data
+
+#creating object of class
+stock=Stock()
+#storing method return value
+
+    
