0cb7ad8c26c52da395b62d43e6ee9c04d68fc064
kishorekumar646 <kishorekumar131646@gmai.com>
2020-01-07 11:44:19 +0530

    [Vikas Sharma]Add. add inventry_management program

---
+++ oops_Program/Inventry_management/inventrymanager.py
@@ -0,0 +1,20 @@
+#*****************************************************
+# @purpose :Read the Json File.
+# @file  :inventoryfactor.py
+# @author :Vikas Sharma
+#*****************************************************
+
+import json
+import inventryfactor
+
+class Inventory:
+    def iventoryCal(self):
+        #Reading JSON from a File
+        with open('/home/user/Videos/test/bridgelabz_python/oops_Program/Inventry_management/inventry_json.json','r') as f:
+
+            #read string from JSON & storing in variable
+            data = json.load(f)
+            print(data)
+
+obj = Inventory()
+obj.iventoryCal()
